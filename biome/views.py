58daa1033c1746ed3f87e4719c66b9ae830de90d
Sandip Chatterjee <sandip.chatterjee@gmail.com>
2015-08-11 19:50:48 -0700

    pulled out plotting view function(s) into separate file for better compartmentalization

--- biome/views.py
+++ biome/views.py
@@ -6,6 +6,7 @@ from flask import ( render_template, jsonify,
                     request
                 )
 from biome import app, api
+from biome.views_plots import *
 
 ## API/Blueprint:
 
@@ -47,62 +48,3 @@ def test_API():
     return json.dumps(json_obj)
 
 
-## simple test with Bokeh -- from example on github: 
-## https://github.com/bokeh/bokeh/tree/master/examples/embed/simple
-
-from bokeh.embed import components
-from bokeh.plotting import figure
-from bokeh.resources import INLINE
-from bokeh.templates import RESOURCES
-from bokeh.util.string import encode_utf8
-
-colors = {
-    'Black': '#000000',
-    'Red':   '#FF0000',
-    'Green': '#00FF00',
-    'Blue':  '#0000FF',
-}
-
-
-def getitem(obj, item, default):
-    if item not in obj:
-        return default
-    else:
-        return obj[item]
-
-@app.route("/bokeh_test")
-def polynomial():
-    """ Very simple embedding of a polynomial chart"""
-    # Grab the inputs arguments from the URL
-    # This is automated by the button
-    args = request.args
-
-    # Get all the form arguments in the url with defaults
-    color = colors[getitem(args, 'color', 'Black')]
-    _from = int(getitem(args, '_from', 0))
-    to = int(getitem(args, 'to', 10))
-
-    # Create a polynomial line graph
-    x = list(range(_from, to + 1))
-    fig = figure(title="Polynomial")
-    fig.line(x, [i ** 2 for i in x], color=color, line_width=2)
-
-    # Configure resources to include BokehJS inline in the document.
-    # For more details see:
-    #   http://bokeh.pydata.org/en/latest/docs/reference/resources_embedding.html#module-bokeh.resources
-    plot_resources = RESOURCES.render(
-        js_raw=INLINE.js_raw,
-        css_raw=INLINE.css_raw,
-        js_files=INLINE.js_files,
-        css_files=INLINE.css_files,
-    )
-
-    # For more details see:
-    #   http://bokeh.pydata.org/en/latest/docs/user_guide/embedding.html#components
-    script, div = components(fig, INLINE)
-    html = render_template(
-        'embed.html',
-        plot_script=script, plot_div=div, plot_resources=plot_resources,
-        color=color, _from=_from, to=to
-    )
-    return encode_utf8(html)
