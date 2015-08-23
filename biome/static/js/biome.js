601ee9a0a5e94b21dafa2788c9b82d958a548e1c
Sandip Chatterjee <sandip.chatterjee@gmail.com>
2015-08-22 18:22:13 -0700

    moved some JS out to external file...

---
+++ biome/static/js/biome.js
@@ -0,0 +1,8 @@
+// String formatting -- from http://stackoverflow.com/a/3492815
+String.prototype.format = function() {
+    var formatted = this;
+    for( var arg in arguments ) {
+        formatted = formatted.replace("{" + arg + "}", arguments[arg]);
+    }
+    return formatted;
+};
