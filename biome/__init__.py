7ede1789a47ef1b42ea6132fb666323e84d83bff
Sandip Chatterjee <sandip.chatterjee@gmail.com>
2015-09-24 14:54:30 -0700

    Moves logging functionality so uWSGI can use it

--- biome/__init__.py
+++ biome/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import logging
 from flask import ( Blueprint, 
                     Flask, 
                     render_template, 
@@ -10,12 +11,25 @@ from celery import Celery
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_wtf import Form
 from biome.config import set_config
+from logging.handlers import RotatingFileHandler
 
 api = Blueprint('api', __name__)
 data = Blueprint('data', __name__)
 search = Blueprint('search', __name__)
 
 app = Flask(__name__, static_folder='static')
+logging_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+info_handler = RotatingFileHandler('log_biome_info.log', maxBytes=10000, backupCount=1)
+info_handler.setLevel(logging.INFO)
+info_handler.setFormatter(logging_formatter)
+
+error_handler = RotatingFileHandler('log_biome_error.log', maxBytes=10000, backupCount=1)
+error_handler.setLevel(logging.ERROR)
+error_handler.setFormatter(logging_formatter)
+
+app.logger.addHandler(info_handler)
+app.logger.addHandler(error_handler)
 set_config(app)
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'])
--- manage.py
+++ manage.py
@@ -3,23 +3,8 @@
 from biome import app, db
 from flask.ext.script import Server, Manager
 from flask.ext.migrate import Migrate, MigrateCommand
-import logging
-from logging.handlers import RotatingFileHandler
 
 if __name__ == '__main__':
-
-    logging_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-    info_handler = RotatingFileHandler('log_biome_info.log', maxBytes=10000, backupCount=1)
-    info_handler.setLevel(logging.INFO)
-    info_handler.setFormatter(logging_formatter)
-
-    error_handler = RotatingFileHandler('log_biome_error.log', maxBytes=10000, backupCount=1)
-    error_handler.setLevel(logging.ERROR)
-    error_handler.setFormatter(logging_formatter)
-
-    app.logger.addHandler(info_handler)
-    app.logger.addHandler(error_handler)
 
     migrate = Migrate(app, db)
 
